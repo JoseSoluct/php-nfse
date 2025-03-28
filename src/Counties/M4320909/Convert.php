dc7681375879510cacf13446116907c997c2ac70
José Alcides <jose.alcides@soluct.com.br>
2025-03-28 16:14:14 -0300

    chore(composer): remover arquivo composer.lock
    O arquivo `composer.lock` foi removido do repositório. Isso permite que a resolução de dependências seja feita de forma mais flexível e facilita a atualização dos pacotes durante o uso do composer. Certifique-se de rodar `composer install` localmente para gerar um novo lockfile se necessário.

---
+++ src/Counties/M4320909/Convert.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M4320909;
+
+/**
+ * Classe a construção do xml da NFSe
+ * para a Cidade de Tapejara RS
+ * conforme o modelo TECNOS
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M4320909\Convert
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Roberto L. Machado <linux.rlm at gmail dot com>
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Tecnos\Convert as ConvertModel;
+
+class Convert extends ConvertModel
+{
+}
